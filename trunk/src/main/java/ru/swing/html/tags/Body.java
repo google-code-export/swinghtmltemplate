44e682349c1a03234220da0dec52cc01e93e9f17
Penkov.Vladimir <Penkov.Vladimir@e5bcafa6-be4c-ee28-ccfd-76c0a0020752>
2010-11-20 22:31:19 +0000

    comments

--- trunk/src/main/java/ru/swing/html/tags/Body.java
+++ trunk/src/main/java/ru/swing/html/tags/Body.java
@@ -7,11 +7,7 @@ import org.apache.commons.logging.LogFactory;
 import javax.swing.*;
 
 /**
- * Created by IntelliJ IDEA.
- * User: deady
- * Date: 20.11.2010
- * Time: 23:53:11
- * To change this template use File | Settings | File Templates.
+ * Тег преобразуется в панель JPanel. По умолчанию подставляется FlowLayout.
  */
 public class Body extends Tag {
 
