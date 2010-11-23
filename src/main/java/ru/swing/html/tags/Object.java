91b5e85b673e8bd865d6d5f80937dbf7ea3d75f6
Penkov.Vladimir <Penkov.Vladimir@e5bcafa6-be4c-ee28-ccfd-76c0a0020752>
2010-11-23 07:47:56 +0000

    Тэг component заменен на object

--- src/main/java/ru/swing/html/tags/Object.java
+++ src/main/java/ru/swing/html/tags/Object.java
@@ -8,22 +8,22 @@ import javax.swing.*;
  * Date: 20.11.2010
  * Time: 23:57:11
  */
-public class Component extends Tag {
+public class Object extends Tag {
 
     @Override
     public JComponent createComponent() {
-        String classname = getType();
+        String classname = getAttribute("classid");
         JComponent c;
         Class cl;
         try {
             cl = Class.forName(classname);
             c = (JComponent) cl.newInstance();
         } catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Can't create component for class "+classname);
+            throw new IllegalArgumentException("Can't create component for class "+classname+": "+e.getMessage());
         } catch (InstantiationException e) {
-            throw new IllegalArgumentException("Can't create component for class "+classname);
+            throw new IllegalArgumentException("Can't create component for class "+classname+": "+e.getMessage());
         } catch (IllegalAccessException e) {
-            throw new IllegalArgumentException("Can't create component for class "+classname);
+            throw new IllegalArgumentException("Can't create component for class "+classname+": "+e.getMessage());
         }
 
         setComponent(c);
