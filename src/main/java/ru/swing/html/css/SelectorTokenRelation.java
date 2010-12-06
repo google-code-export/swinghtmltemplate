665b72160a020cd3b0213a8b5eee6227681ee00b
Penkov.Vladimir <Penkov.Vladimir@e5bcafa6-be4c-ee28-ccfd-76c0a0020752>
2010-12-06 21:07:32 +0000

    started work on full css selector support

---
+++ src/main/java/ru/swing/html/css/SelectorTokenRelation.java
@@ -0,0 +1,18 @@
+package ru.swing.html.css;
+
+/**
+ * Тип связи от текущего токена к следующему.
+ */
+public enum SelectorTokenRelation {
+    ANY(""), PARENT(">"), SUBLING("+");
+
+    private String symbol;
+
+    SelectorTokenRelation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
