84fa8e88d017b22f191f0f6273f786071b16a950
Penkov.Vladimir <Penkov.Vladimir@e5bcafa6-be4c-ee28-ccfd-76c0a0020752>
2011-06-09 09:55:33 +0000

    'onchange' event support in 'j:list'

--- trunk/sht-core/src/test/java/ru/swing/html/tags/swing/ListTest.java
+++ trunk/sht-core/src/test/java/ru/swing/html/tags/swing/ListTest.java
@@ -4,30 +4,31 @@ import junit.framework.TestCase;
 import ru.swing.html.*;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
 import java.awt.event.ActionEvent;
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 
-public class ComboboxTest extends TestCase {
+public class ListTest extends TestCase {
 
     public void testCreateComponent() throws Exception {
-        Combobox combobox = new Combobox();
+        List combobox = new List();
         JComponent c = combobox.createComponent();
         assertNotNull(c);
-        assertTrue(c instanceof JComboBox);
+        assertTrue(c instanceof JList);
     }
 
 
     public void testOnchange() throws Exception {
 
-        TestComboboxChange controller = new TestComboboxChange();
+        TestListChange controller = new TestListChange();
         String html =
                 "<html xmlns:j=\"http://www.oracle.com/swing\">\n" +
                 "<head></head>\n" +
                 "<body>\n" +
-                "   <j:combobox id='b' onchange='foo'>\n" +
+                "   <j:list id='b' onchange='foo'>\n" +
                 "       <j:selectItems value=\"${items}\" />\n" +
-                "   </j:combobox>" +
+                "   </j:list>" +
                 "</body>\n" +
                 "</html>";
         DomModel model = DomLoader.loadModel(new ByteArrayInputStream(html.getBytes()));
@@ -36,26 +37,37 @@ public class ComboboxTest extends TestCase {
         assertNull(controller.text);
 
         Binder.bind(controller, false, model);
-        JComboBox b = (JComboBox) model.getTagById("b").getComponent();
+        JList b = (JList) model.getTagById("b").getComponent();
 
         b.setSelectedIndex(0);
         assertEquals("foo1", controller.text);
+        assertEquals(1, controller.selectedCount);
 
         b.setSelectedIndex(1);
         assertEquals("foo2", controller.text);
+        assertEquals(1, controller.selectedCount);
+
+        b.setSelectedIndices(new int[] {0, 1});
+        assertEquals(2, controller.selectedCount);
 
     }
 
-    public class TestComboboxChange {
+    public class TestListChange {
 
         public String text = null;
+        public int selectedCount = 0;
+
+        @Bind("b")
+        private JList list;
 
         @ModelElement("items")
         private java.util.List<String> items = Arrays.asList("foo1", "foo2");
 
-        public void foo(ActionEvent e) {
-            JComboBox b = (JComboBox) e.getSource();
-            text = (String) b.getSelectedItem();
+        public void foo(ListSelectionEvent e) {
+            text = (String) list.getSelectedValue();
+            selectedCount = list.getSelectedIndices().length;
         }
     }
+
+
 }
